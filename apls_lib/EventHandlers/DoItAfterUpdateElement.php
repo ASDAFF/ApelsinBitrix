f967b233278a44043f722b54f8a281c763511a5c
Maxim Zaitsev <zaytsev.max90@gmail.com>
2016-09-27 16:56:54 +0300

    APLS библиотеки и ресурсы

---
+++ apls_lib/EventHandlers/DoItAfterUpdateElement.php
@@ -0,0 +1,32 @@
+<?php
+AddEventHandler("iblock", "OnAfterIBlockElementUpdate", "DoItAfterUpdateElement");
+AddEventHandler("iblock", "OnAfterIBlockElementAdd", "DoItAfterUpdateElement");
+
+define("SHOP_IBLOC_ID_FOR_THIS_SCRIPT", "31");
+define("ACTIVE_TRIGGER_PROPERTY_XML_ID", "26e05687-c602-4c36-8b63-debb1b4e0250");
+define("ACTIVE_TRIGGER_PROPERTY_VALUE_DEACTIVE", "Нет");
+define("ACTIVE_TRIGGER_PROPERTY_VALUE_ACTIVE", "Да");
+
+function DoItAfterUpdateElement (&$arFields) {
+	if ($arFields["IBLOCK_ID"] == SHOP_IBLOC_ID_FOR_THIS_SCRIPT && $arFields["RESULT"]) {
+		$rs = CIBlockElement::GetList(array(), array("ID"=>$arFields["ID"]), false, array("nTopCount"=>1));
+		if($obRes = $rs->GetNextElement())
+		{
+			$arRes = $obRes->GetFields();
+			$arRes["PROPERTIES"] = $obRes->GetProperties();
+			foreach ($arRes["PROPERTIES"] as $key => $val) {
+				if($val["XML_ID"] === ACTIVE_TRIGGER_PROPERTY_XML_ID) {
+					if($val["VALUE"] === ACTIVE_TRIGGER_PROPERTY_VALUE_DEACTIVE && $arRes["ACTIVE"] === "Y") {
+						$el = new CIBlockElement;
+						$arLoadProductArray = Array("ACTIVE" => "N");
+						$el->Update($arFields["ID"], $arLoadProductArray);
+					} elseif ($val["VALUE"] === ACTIVE_TRIGGER_PROPERTY_VALUE_ACTIVE && $arRes["ACTIVE"] === "N") {
+						$el = new CIBlockElement;
+						$arLoadProductArray = Array("ACTIVE" => "Y");
+						$el->Update($arFields["ID"], $arLoadProductArray);
+					}
+				}
+			}
+		}
+	}
+}
---
+++ apls_lib/apls_lib.php
@@ -0,0 +1,24 @@
+<?php
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
+function includeSistemClasses($path = './') {
+	include_once $path.'apls_lib/main/textgenerator/ID_GENERATOR.php';
+	include_once $path.'apls_lib/main/parser/AplsXmlParser.php';
+
+	include_once $path.'apls_lib/contacts/ContactsMain.php';
+	include_once $path.'apls_lib/contacts/DataRegionsShop.php';
+	include_once $path.'apls_lib/contacts/DisplayPageContactsShop.php';
+	include_once $path.'apls_lib/contacts/DataContactsShop.php';
+	include_once $path.'apls_lib/contacts/ContactsTimeTableUI.php';
+	include_once $path.'apls_lib/contacts/DisplayContactsShop.php';
+
+	include_once $path.'apls_lib/contacts/DataContactsWokers.php';
+	include_once $path.'apls_lib/contacts/DisplayContactsWokers.php';
+
+	include_once $path.'apls_lib/service_centers/APLS_ServiceCenters.php';
+
+	include_once $path.'apls_lib/vacancies/APLS_Data_Vacancies.php';
+	include_once $path.'apls_lib/vacancies/APLS_View_Vacancies.php';
+	include_once $path.'apls_lib/vacancies/APLS_Controller_Vacancies.php';
+
+}
+?>
