b08c26a57d1d4794d4931a0201315bf5e6ffc913
Maxim Zaitsev <zaytsev.max90@gmail.com>
2016-09-27 17:08:50 +0300

    Замена логотипа

--- include/company_logo.php
+++ include/company_logo.php
@@ -1,14 +1,19 @@
 <?global $arSetting;?>
 
-<a href="<?=SITE_DIR?>">	
+
+
+<a href="<?=SITE_DIR?>">
+	<img src="<?=SITE_TEMPLATE_PATH?>/images/apelsin_logo.png" alt="logo" />
+	<!--
 	<?if($arSetting["COLOR_SCHEME"]["VALUE"] == "YELLOW"):?>
-		<img src="<?=SITE_TEMPLATE_PATH?>/images/logo.png" alt="logo" />
+		<img src="<?=SITE_TEMPLATE_PATH?>/images/apelsin_logo.png" alt="logo" />
 	<?else:
 		if($arSetting["SITE_BACKGROUND"]["VALUE"] == "DARK"):?>
-			<img src="<?=SITE_TEMPLATE_PATH?>/images/logo_dark.png" alt="logo" />
+			<img src="<?=SITE_TEMPLATE_PATH?>/images/apelsin_logo.png" alt="logo" />
 		<?else:?>
-			<img src="<?=SITE_TEMPLATE_PATH?>/images/logo_gray.png" alt="logo" />
+			<img src="<?=SITE_TEMPLATE_PATH?>/images/apelsin_logo.png" alt="logo" />
 		<?endif;
 	endif;?>
-	<span>Готовый интернет-магазин на 1С-Битрикс</span>
+	<span>Строительство и ремонт</span>
+	-->
 </a>
