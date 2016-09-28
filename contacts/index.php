62f6be3956c427376d792ea3a0b2fd532a61251d
Maxim Zaitsev <zaytsev.max90@gmail.com>
2016-09-28 15:04:33 +0300

    Добавление страницы - contacts

---
+++ contacts/index.php
@@ -0,0 +1,35 @@
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Контакты");?>
+    <div class="NavigatorBigIcon">
+        <a href="shops/index.php" class="NavigatorBigIconElementHref">
+            <div class="NavigatorBigIconElement">
+                <img class="main" src="img/icon/shops.png">
+                <img class="hover" src="img/icon/shops_hover.png">
+                <div class="title">Магазины</div>
+            </div>
+        </a>
+        <a href="point_of_delivery/index.php" class="NavigatorBigIconElementHref">
+            <div class="NavigatorBigIconElement">
+                <img class="main" src="img/icon/point_of_delivery.png">
+                <img class="hover" src="img/icon/point_of_delivery_hover.png">
+                <div class="title">Пункты выдачи</div>
+            </div>
+        </a>
+        <a href="suppliers/index.php" class="NavigatorBigIconElementHref">
+            <div class="NavigatorBigIconElement">
+                <img class="main" src="img/icon/suppliers.png">
+                <img class="hover" src="img/icon/suppliers_hover.png">
+                <div class="title">Отдел закупок</div>
+            </div>
+        </a>
+        <a href="studio_furniture/index.php" class="NavigatorBigIconElementHref">
+            <div class="NavigatorBigIconElement">
+                <img class="main" src="img/icon/studio_furniture.png">
+                <img class="hover" src="img/icon/studio_furniture_hover.png">
+                <div class="title">Студия мебели</div>
+            </div>
+        </a>
+        <div class="clear"></div>
+    </div>
+
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php")?>
