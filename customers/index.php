e9c0cf76dbc695e680f2c7f5fb2d4b91a290d841
Maxim Zaitsev <zaytsev.max90@gmail.com>
2016-09-28 15:43:17 +0300

    Добавление страницы customers

---
+++ customers/index.php
@@ -0,0 +1,44 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Покупателям");
+?>
+	<div class="NavigatorBigIcon">
+		<a href="discount/" class="NavigatorBigIconElementHref">
+			<div class="NavigatorBigIconElement">
+				<img class="main" src="img/icon/discount.png">
+				<img class="hover" src="img/icon/discount_hover.png">
+				<div class="title">Дисконт</div>
+			</div>
+		</a>
+		<a href="gift_cards/" class="NavigatorBigIconElementHref">
+			<div class="NavigatorBigIconElement">
+				<img class="main" src="img/icon/gift_cards.png">
+				<img class="hover" src="img/icon/gift_cards_hover.png">
+				<div class="title">Подарочные карты</div>
+			</div>
+		</a>
+		<a href="weekend_discount/" class="NavigatorBigIconElementHref">
+			<div class="NavigatorBigIconElement">
+				<img class="main" src="img/icon/weekend_discount.png">
+				<img class="hover" src="img/icon/weekend_discount_hover.png">
+				<div class="title">Скидка выходного дня</div>
+			</div>
+		</a>
+		<a href="bonus_program/" class="NavigatorBigIconElementHref">
+			<div class="NavigatorBigIconElement">
+				<img class="main" src="img/icon/bonus_program.png">
+				<img class="hover" src="img/icon/bonus_program_hover.png">
+				<div class="title">Программа бонус</div>
+			</div>
+		</a>
+		<a href="credit/" class="NavigatorBigIconElementHref">
+			<div class="NavigatorBigIconElement">
+				<img class="main" src="img/icon/credit.png">
+				<img class="hover" src="img/icon/credit_hover.png">
+				<div class="title">Кредит</div>
+			</div>
+		</a>
+		<div class="clear"></div>
+	</div>
+
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
