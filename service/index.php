9293b2894bfb4dcb25eccbbd65bd9ce1f2f9523a
Maxim Zaitsev <zaytsev.max90@gmail.com>
2016-09-29 10:57:26 +0300

    добавление страницы service

---
+++ service/index.php
@@ -0,0 +1,57 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Услуги");
+?>
+
+	<div class="NavigatorBigIcon">
+		<a href="../delivery/" class="NavigatorBigIconElementHref">
+			<div class="NavigatorBigIconElement">
+				<img class="main" src="img/icon/delivery.png">
+				<img class="hover" src="img/icon/delivery_hover.png">
+				<div class="title">Доставка</div>
+			</div>
+		</a>
+		<a href="../delivery/index.php#climbInfo" class="NavigatorBigIconElementHref">
+			<div class="NavigatorBigIconElement">
+				<img class="main" src="img/icon/climb.png">
+				<img class="hover" src="img/icon/climb_hover.png">
+				<div class="title">Подъем</div>
+			</div>
+		</a>
+		<a href="tinting/" class="NavigatorBigIconElementHref">
+			<div class="NavigatorBigIconElement">
+				<img class="main" src="img/icon/tinting.png">
+				<img class="hover" src="img/icon/tinting_hover.png">
+				<div class="title">Колеровка</div>
+			</div>
+		</a>
+		<a href="door_installation/" class="NavigatorBigIconElementHref">
+			<div class="NavigatorBigIconElement">
+				<img class="main" src="img/icon/door_installation.png">
+				<img class="hover" src="img/icon/door_installation_hover.png">
+				<div class="title">Установка дверей и ворот</div>
+			</div>
+		</a>
+		<a href="sanitary/" class="NavigatorBigIconElementHref">
+			<div class="NavigatorBigIconElement">
+				<img class="main" src="img/icon/sanitary.png">
+				<img class="hover" src="img/icon/sanitary_hover.png">
+				<div class="title">Установка душевых кабин, ванн, санфаянса</div>
+			</div>
+		</a>
+		<a href="conditioners/" class="NavigatorBigIconElementHref">
+			<div class="NavigatorBigIconElement">
+				<img class="main" src="img/icon/conditioners.png">
+				<img class="hover" src="img/icon/conditioners_hover.png">
+				<div class="title">Установка кондиционеров</div>
+			</div>
+		</a>
+		<a href="gasBoilers/" class="NavigatorBigIconElementHref">
+			<div class="NavigatorBigIconElement">
+				<img class="main" src="img/icon/gasBoilers.png">
+				<img class="hover" src="img/icon/gasBoilers_hover.png">
+				<div class="title">Установка газовых котлов</div>
+			</div>
+		</a>
+	</div>
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
