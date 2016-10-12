644415d308bb688a6f121d43e5cfb79848cd97f3
Maxim Zaitsev <zaytsev.max90@gmail.com>
2016-10-12 10:21:13 +0300

    подключение файлов APLS_TextInspections и APLS_TextGenerator

--- apls_lib/apls_lib.php
+++ apls_lib/apls_lib.php
@@ -1,8 +1,10 @@
 <?php
 if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
 function includeSistemClasses($path = './') {
-	include_once $path.'apls_lib/main/textgenerator/ID_GENERATOR.php';
+	include_once $path.'apls_lib/main/inspections/APLS_TextInspections.php';
 	include_once $path.'apls_lib/main/parser/AplsXmlParser.php';
+	include_once $path.'apls_lib/main/textgenerator/ID_GENERATOR.php';
+	include_once $path.'apls_lib/main/textgenerator/APLS_TextGenerator.php';
 
 	include_once $path.'apls_lib/contacts/ContactsMain.php';
 	include_once $path.'apls_lib/contacts/DataRegionsShop.php';
