6a2e7016388d47e2ffe30ccd0e0260b154d3937b
Olga Rjabchikova <olga.rjabchicova@gmail.com>
2017-01-11 10:21:00 +0300

    AST-96 Переход на страницу с контактами

--- bitrix/templates/elektro_flat/components/bitrix/catalog.store.amount/.default/template.php
+++ bitrix/templates/elektro_flat/components/bitrix/catalog.store.amount/.default/template.php
@@ -10,11 +10,26 @@ if(count($arResult["STORES"]) > 0):
 		<?if($arProperty["AMOUNT"] > 0):?>
 		<div class="catalog-detail-store">			
 			<span class="name">
-				<?=$arProperty["TITLE"].(isset($arProperty["PHONE"]) ? GetMessage("S_PHONE").$arProperty["PHONE"] : "").(isset($arProperty["SCHEDULE"]) ? GetMessage("S_SCHEDULE").$arProperty["SCHEDULE"] : "");?>
-				<?=($arParams["SHOW_GENERAL_STORE_INFORMATION"] == "Y") ? GetMessage("S_BALANCE") : "";?>				
+<!--				--><?//=$arProperty["TITLE"].(isset($arProperty["PHONE"]) ? GetMessage("S_PHONE").$arProperty["PHONE"] : "").(isset($arProperty["SCHEDULE"]) ? GetMessage("S_SCHEDULE").$arProperty["SCHEDULE"] : "");?>
+<!--				--><?//=($arParams["SHOW_GENERAL_STORE_INFORMATION"] == "Y") ? GetMessage("S_BALANCE") : "";?><!--				-->
 			</span>
-			<span class="val"><?=$arProperty["AMOUNT"]?></span>
-		</div>
+			<a href="../../../../../../../contacts/shops" target="_blank">
+				<div class="store">
+					<div class="title">
+						<?=$arProperty["TITLE"]?>
+					</div>
+					<div class="phone">
+						<?=isset($arProperty["PHONE"]) ? $arProperty["PHONE"] : ""?>
+					</div>
+				</div>
+				<div class="adres">
+					<?=isset($arProperty["SCHEDULE"]) ? $arProperty["SCHEDULE"] : ""?>
+				</div>
+				<span class="amount"><?=$arProperty["AMOUNT"]?></span>
+
+			</a>
+	<!--			<span class="val">--><?//=$arProperty["AMOUNT"]?><!--</span>-->
+			</div>
 		<?endif;?>
 	<?endforeach;
 endif;?>
